dc63c59f339942758aca7c788f4cd7f9a702f362
DESKTOP-QBLRO6P\HLC_2021 <hieund@horusent.com>
2021-12-07 17:54:57 +0700

    update 07/12/2021

--- Assets/Script/CarMovement.cs
+++ Assets/Script/CarMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
@@ -8,28 +8,95 @@ public class CarMovement : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] Transform attackPos;
+
     [SerializeField] Transform target;
+    [SerializeField] Transform shootPos;
+
+    [SerializeField] GameObject bullet;
+
+    [SerializeField] float shootSpeed;
+    [SerializeField] Transform machineGun;
+    [SerializeField] float rotateSpeed;
 
     NavMeshAgent agent;
 
+    bool attacked = false;// biến check xem machine gun đã bắn chưa
+
+
     private void Start()
     {
+        //tìm vị trí của attack pos
         attackPos = GameObject.FindGameObjectWithTag("AttackPos").transform;
 
         agent = GetComponent<NavMeshAgent>();
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(attackPos.position);
+        agent.SetDestination(attackPos.position);//đặt điểm đến cho agent
+
+        
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "AttackPos")
+        if (other.gameObject.tag == "AttackPos") //nếu chạm vào game object có tag là AttackPos thì thực hiện xoay vào bắn
         {
-            transform.LookAt(target);
+            Debug.Log(attacked);
+
+            if (RotateMachineGun() && !attacked)// nếu hàm RotateMachineGun trả về true, và biến chưa đánh trả true
+            {
+                attacked = true;
+                StartCoroutine(Wait2Shoot());
+            }
+
         }
     }
+
+    private void Shoot()
+    {
+        GameObject bulletObject = Instantiate(bullet, shootPos.position, shootPos.rotation); //sinh viên đạnn
+
+        Rigidbody bulletRB = bulletObject.GetComponent<Rigidbody>();// lấy rigidbody của viên đạn
+
+        bulletRB.AddForce(bulletRB.transform.forward * shootSpeed, ForceMode.Impulse);// thực hiện bắn
+
+        Destroy(bulletObject, 1f); //xóa bullet
+
+    }
+
+    private bool RotateMachineGun()
+    {
+        Vector3 targetDirection = target.position - machineGun.position; //hướng của target
+
+        float speed = rotateSpeed * Time.deltaTime;// tính tốc độ xoay
+
+        Vector3 newDirection = Vector3.RotateTowards(machineGun.forward, targetDirection, speed, 0f); //chỉ ra hướng của mục tiêu
+
+        Debug.DrawRay(machineGun.position, newDirection, Color.red); //vẽ một đường ray cast chỉ đến mục tiêu
+
+        //check true false, nếu chỉ đến turret thì trả về true ???
+
+        machineGun.rotation = Quaternion.LookRotation(newDirection);//thực hiện quay
+
+        return true;//trả về true nếu đã quay đến hướng turret
+    }
+
+    IEnumerator Wait2Shoot()
+    {
+        yield return new WaitForSeconds(4f);// đợi 1 s rồi bắn
+
+        Shoot();
+
+        yield return new WaitForSeconds(4f); //đợi 1 s rồi thực hiện tìm mục tiêu khác để di chuyển đến
+
+        attacked = false;
+
+        agent.SetDestination(attackPos.position);//đặt điểm đến cho agent
+
+    }
+
 }
---
+++ Assets/Script/MachinGunScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinGunScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        //Vector3 targetDirection = target.position = machineGun.position;
+
+        //float speed = rotateSpeed * Time.deltaTime;
+
+        //Vector3 newDirection = Vector3.RotateTowards(machineGun.forward, targetDirection, speed, 0f);
+
+        //Debug.DrawRay(machineGun.position, newDirection, Color.red);
+
+        //machineGun.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
